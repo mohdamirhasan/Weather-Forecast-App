7b6793a00253c75b6c575af540b255a2fda84f55
Mohd Amir Hasan <meetamirhasan@gmail.com>
2024-10-15 17:53:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -33,43 +33,42 @@ async function getWeather(city) {
   }
 }
 
-search_button = document.getElementById('search');
-function weather(){
-  console.log("clicked")
-    const info = document.getElementById("info");
-    info.style.display = "none";
-    document.getElementById("error").style.display = "none";
-    let city = document.getElementById("input").value;
-    city = titleCase(city);
-    getWeather(city).then((weatherData) => {
-      if (weatherData != -1) {
-          box.style.height = "max-content";
-          info.style.display = "flex";
-          document.getElementById("error").style.display = "none";
-          const temperature = weatherData.temp;
-          const description = weatherData.description;
-          const windsp = Number(weatherData.windSpeed);
-          const Icon = weatherData.Icon;
-          const weathericon = document.getElementById("weather-icon");
-          document.getElementById("temp").innerHTML = `${temperature}°C`;
-          document.getElementById("city").innerHTML = weatherData.cityName;
-          icon_url = `https://openweathermap.org/img/wn/${Icon}@2x.png`;
-          weathericon.src = icon_url;
-          document.getElementById("descrip").innerHTML = titleCase(description);
-          document.getElementById("speed").innerHTML = `Wind: ${windsp} m/s`;
-          
-      } else if (weatherData == -1) {
-          box.style.height = "max-content";
-          info.style.display = "none";
-          document.getElementById("error").style.display = "block";
-          info.style.fontSize = "20px";
-      }
-    });
-  }
-search_button.addEventListener('click',weather);
+const search_button = document.getElementById("search");
+function weather() {
+  console.log("clicked");
+  const info = document.getElementById("info");
+  info.style.display = "none";
+  document.getElementById("error").style.display = "none";
+  let city = document.getElementById("input").value;
+  city = titleCase(city);
+  getWeather(city).then((weatherData) => {
+    if (weatherData != -1) {
+      box.style.height = "max-content";
+      info.style.display = "flex";
+      document.getElementById("error").style.display = "none";
+      const temperature = weatherData.temp;
+      const description = weatherData.description;
+      const windsp = Number(weatherData.windSpeed);
+      const Icon = weatherData.Icon;
+      const weathericon = document.getElementById("weather-icon");
+      document.getElementById("temp").innerHTML = `${temperature}°C`;
+      document.getElementById("city").innerHTML = weatherData.cityName;
+      icon_url = `https://openweathermap.org/img/wn/${Icon}@2x.png`;
+      weathericon.src = icon_url;
+      document.getElementById("descrip").innerHTML = titleCase(description);
+      document.getElementById("speed").innerHTML = `Wind: ${windsp} m/s`;
+    } else if (weatherData == -1) {
+      box.style.height = "max-content";
+      info.style.display = "none";
+      document.getElementById("error").style.display = "block";
+      info.style.fontSize = "20px";
+    }
+  });
+}
+search_button.addEventListener("click", weather);
 document.addEventListener("keydown", function (event) {
   if (event.code === "Enter") {
-    document.getElementById("search").click();
-    search_button.addEventListener('click',weather);
+    search_button.click();
+    search_button.addEventListener("click", weather);
   }
 });
